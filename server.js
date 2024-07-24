ec8ce2a7c440334d00c59ad8f52bf9cb69ee8da3
subhankar <subhankarsahoo030@gmail.com>
2024-07-25 00:28:06 +0530

    some changes in deployement

--- server.js
+++ server.js
@@ -28,12 +28,13 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 //static files:
-app.use(express.static(path.join(__dirname, "./client/build")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
-
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 const PORT = process.env.PORT || 8080;
 //start the server:
 app.listen(PORT, () => {
